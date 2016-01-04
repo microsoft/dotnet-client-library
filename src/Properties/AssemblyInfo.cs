d242c36245fdc3f767247495ef67ce5d56e17f12
dec100 <davchamp@microsoft.com>
2016-01-04 13:58:45 -0800

    Update to Visual Studio 2013

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DeployR7.4")]
+[assembly: AssemblyTitle("DeployR8.0")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Revolution Analytics Inc")]
-[assembly: AssemblyProduct("DeployR7.4")]
-[assembly: AssemblyCopyright("Copyright ©2010-2015,  Revolution Analytics Inc.")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("DeployR8.0")]
+[assembly: AssemblyCopyright("Copyright ©2010-2015, Microsoft Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("7.4.4.0")]
-[assembly: AssemblyFileVersion("7.4.4.0")]
+[assembly: AssemblyVersion("8.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
